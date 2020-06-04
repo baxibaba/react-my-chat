128e3a5a451d36be93834948e907ce30e119edc3
baxi <baxibaba@126.com>
2020-06-04 12:00:11 +0800

    init react project

--- src/index.js
+++ src/index.js
@@ -4,14 +4,43 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
+
+import Login from './container/login/login'
+import Register from './container/register/register'
+import AuthRoute from './component/authroute/authroute'
+import reducers from './reducer'
+import './config'
+import './index.css'
+const store = createStore(reducers, compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension?window.devToolsExtension():f=>f
+))
+function Boss(){
+	return <h2>BOSS12baxi.png页面</h2>
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  (<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<AuthRoute></AuthRoute>
+				<Route path='/boss' component={Boss}></Route>
+				<Route path='/login' component={Login}></Route>
+				<Route path='/register' component={Register}></Route>
+			</div>
+		</BrowserRouter>
+	</Provider>),
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
